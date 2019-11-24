a487a5444523ad6c22ec6f21243a443c678d5f41
Zac Heisey <zheisey@gmail.com>
2019-11-24 12:56:09 -0500

    continue testing netlify functions

--- functions/netlify-function.js
+++ functions/netlify-function.js
@@ -1,9 +1,8 @@
-const { TEST_VAR_1 } = process.env.TEST_VAR_1;
-const { TEST_VAR_2 } = process.env.TEST_VAR_2;
+const { TEST_VAR_1 } = process.env;
 
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: TEST_VAR_1 + TEST_VAR_2
+    body: TEST_VAR_1
   };
 };
