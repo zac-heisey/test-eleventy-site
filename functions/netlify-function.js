c11d7c5dc58f6aedbdc9a16e50210c83b91e59b5
Zac Heisey <zheisey@gmail.com>
2019-11-24 14:41:38 -0500

    more function testing

--- functions/netlify-function.js
+++ functions/netlify-function.js
@@ -1,13 +1,25 @@
 const fetch = require('node-fetch');
 
-const API_URL = process.env.API_URL;
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
 exports.handler = async (event, context) => {
-  return fetch(API_URL)
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
+}
