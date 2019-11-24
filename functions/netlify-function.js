48ceb72082cd660a8ff7e92b7f16dc85b5dd191f
Zac Heisey <zheisey@gmail.com>
2019-11-24 14:49:21 -0500

    more function testing

--- functions/netlify-function.js
+++ functions/netlify-function.js
@@ -1,13 +1,9 @@
-const fetch = require('node-fetch');
-
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const TEST_VAR_2 = process.env.TEST_VAR_2;
+const API_URL = process.env.API_URL;
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+  return {
+    statusCode: 200,
+    body: `TEST_VAR_2: ${TEST_VAR_2} | API_URL: ${API_URL}`
+  };
 };
