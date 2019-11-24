c9d0eb0f19d0c4a880c4731eedbf55cdf4a50c71
Zac Heisey <zheisey@gmail.com>
2019-11-24 14:37:37 -0500

    more function testing

--- functions/netlify-function.js
+++ functions/netlify-function.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const API_URL = process.env.API_URL;
 
 exports.handler = async (event, context) => {
-  return fetch(API_URL, { headers: { "Accept": "application/json" } })
+  return fetch(API_URL)
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
