d5a663f28e8ac3116c003761e9165721d624c181
Zac Heisey <zheisey@gmail.com>
2019-11-24 11:22:24 -0500

    continue env variable testing

--- js/test.js
+++ js/test.js
@@ -1,4 +1,15 @@
 // Test env variables
 require('dotenv').config();
 
-console.log(process.env);
+console.log('Set in .env file: ' + process.env.TEST_VAR_1);
+console.log('Set in Netlify UI: ' + process.env.TEST_VAR_2);
+
+const mainH2 = document.querySelector('#main-content > h2');
+
+fetch(process.env.API_URL).then((response) => {
+  return response.json();
+}).then((data) => {
+  mainH2.innerText = data[3].title;
+}).catch((err) => {
+  console.warn('Something went wrong :(', err);
+});
